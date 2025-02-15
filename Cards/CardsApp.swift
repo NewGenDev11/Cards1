a3d42f8e6e9265f73566e1e75167390f19ccd168
NewGenDev11 <fisher.curtis85@gmail.com>
2025-02-15 17:43:10 -0500

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Curtis Fisher on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
